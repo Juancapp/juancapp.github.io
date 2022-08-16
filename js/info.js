2fa840c0c457115db693c692c0a52f86cfdbfcfd
juancapp <juanitocamo3@gmail.com>
2022-08-16 02:39:30 -0300

    probandoo

---
+++ js/info.js
@@ -0,0 +1,10 @@
+let juan = document.getElementById('juan')
+
+juan.addEventListener('mouseover', (event) => {
+    element = event.target
+    return juan.innerHTML = 'inicio'
+})
+juan.addEventListener('mouseout', (event) => {
+    element = event.target
+    juan.innerHTML = '.juan'
+})
---
+++ js/script.js
@@ -0,0 +1,76 @@
+const $hola = document.getElementById('hola')
+const $soy = document.getElementById('soy')
+const $juan = document.getElementById('juan')
+
+const $container = document.querySelector('.econtainer')
+
+const hover = event => {
+	element = event.target
+
+	if (element === $hola) return ($hola.innerHTML = 'Info.')
+	if (element === $soy) return ($soy.innerHTML = 'Portfolio')
+	if (element === $juan) return ($juan.innerHTML = 'Contacto')
+}
+
+const over = event => {
+	element = event.target
+
+	if (element === $hola) return ($hola.innerHTML = 'Hola.')
+	if (element === $soy) return ($soy.innerHTML = 'Soy')
+	if (element === $juan) return ($juan.innerHTML = 'Juan')
+}
+
+$hola.addEventListener('mouseover', hover)
+$soy.addEventListener('mouseover', hover)
+$juan.addEventListener('mouseover', hover)
+
+$hola.addEventListener('mouseout', over)
+$soy.addEventListener('mouseout', over)
+$juan.addEventListener('mouseout', over)
+
+$hola.addEventListener('click', () => {
+	window.open('../html/info.html', '_blank')
+})
+
+$soy.addEventListener('click', () => {
+	window.open('../html/portfolio.html', '_blank')
+})
+
+$juan.addEventListener('click', () => {
+	window.open('../html/contacto.html', '_blank')
+})
+
+var mobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i)
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i)
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i)
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i)
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i)
+	},
+	any: function () {
+		return (
+			mobile.Android() || mobile.BlackBerry() || mobile.iOS() || mobile.Opera() || mobile.Windows()
+		)
+	},
+}
+
+document.addEventListener('click', e => {
+	if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i)) {
+		console.log('click')
+
+		$container.classList.add('active')
+
+		$hola.textContent = 'Info.'
+		$soy.textContent = 'Portfolio'
+		$juan.textContent = 'Contacto'
+	}
+})
