619e5d8b86a0896b26142139984f03c664c2c6b3
Juancapp <juan.cappuccio@radiumrocket.com>
2023-10-22 20:08:16 -0300

    feature: first navbar

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="flex flex-col h-screen w-screen bg-white">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const navigation: { name: string; href: string; current: boolean }[] = [
+  { name: "Home", href: "#", current: true },
+  { name: "Project", href: "#", current: false },
+  { name: "Contact", href: "#", current: false },
+];
