b725edefc6f2b210349ebe1cf87643774f79bb4a
Juan Andrés Cappuccio <98498931+Juancapp@users.noreply.github.com>
2025-05-10 21:05:11 -0300

    Update constants.ts
    add Angular

--- src/Components/Stack/constants.ts
+++ src/Components/Stack/constants.ts
@@ -1,15 +1,19 @@
 export const images: { src: string; alt: string }[] = [
   {
+    src: "https://miro.medium.com/v2/resize:fit:640/format:webp/0*p4OJ29rbtqDvpLU7.png",
+    alt: "React",
+  },
+  {
+    src: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*1HXCJCOpzKdmQI33ZrEIlg.png",
+    alt: "Angular",
+  },
+  {
     src: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/MongoDB_Logo.svg/512px-MongoDB_Logo.svg.png",
     alt: "MongoDB",
   },
   {
     src: "images/express-js.webp",
     alt: "ExpressJS",
-  },
-  {
-    src: "https://miro.medium.com/v2/resize:fit:640/format:webp/0*p4OJ29rbtqDvpLU7.png",
-    alt: "React",
   },
   {
     src: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg",
